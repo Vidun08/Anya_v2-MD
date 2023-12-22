6a99d726f0329fee4de94b63de17ad3de51e299e
Vidun08 <153569262+Vidun08@users.noreply.github.com>
2023-12-22 06:10:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,21 +1,6 @@
 const { readFileSync } = require('fs')
 require("dotenv").config();
 
-global.botname = process.env.BotName || "Queen Anya" 
-global.author = process.env.Author || "@VidunDinsara" 
-global.packname = process.env.PackName || "Queen Anya v2 MD" 
-global.myweb = process.env.Web || "https://github.com/PikaBotz" 
-global.footer = process.env.Footer || "© Queen Anya Bot" 
-global.prefa = process.env.Prefix || ['-'] 
-global.themeemoji = process.env.ThemeEmoji || "🎐" 
-global.ownername = process.env.Owner_Name || "Vidun~Dinsara" 
-global.ownernumber = process.env.Owner_Number || "94722348594" 
-global.adress = process.env.Continent || "Asia,Sri Lanka, Matara" 
-global.timezone = process.env.TimeZone || "Asia/Colombo" 
-global.instagramId = process.env.Insta || "8.08_only_mine" 
-global.email = process.env.Email_Id || "renukasriyangani14@gmail.com" 
-
-
 let badWords = [
   "vagina",
   "dick",
@@ -61,19 +46,6 @@ let badWords = [
   "laund",
   "loda",
   "gulambi"];
-  
-//--------------- Tip ----------------\\
-global.Tips = [
-`Type *$prefix info* for more information....`,
-`Type *$prefix settings* to commit changes in the bot.`,
-`If you got a bug or error, then please report to developer asap by *$prefix report* command.`
-]
-
-//--------------- Menu images ----------------\\
-global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
-global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
-global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
-global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
 
 global.message = {
     success: "✅ 𝚂𝚞𝚌𝚌𝚎𝚜𝚜! 𝙾𝚙𝚛𝚊𝚝𝚒𝚘𝚗 𝙲𝚘𝚖𝚙𝚕𝚎𝚝𝚎𝚍.",
@@ -92,30 +64,25 @@ global.message = {
 
 module.exports = {
   botname: process.env.BotName || "Queen Anya", 
-  author: process.env.Author || "@VidunDinsara",
+  author: process.env.Author || "@PikaBotz",
   packname: process.env.PackName || "Queen Anya v2 MD",
-  myweb: process.env.Web || "https://github.com/PikaBotz",
+  socialLink: process.env.Web || "https://github.com/PikaBotz",
   footer: process.env.Footer || "© Queen Anya Bot",
   prefa: process.env.Prefix || ['-'],
   themeemoji: process.env.ThemeEmoji || "🎐",
-  ownername: process.env.Owner_Name || "Vidun~Dinsara",
-  ownernumber: process.env.Owner_Number || "94722348594",
+  ownername: process.env.Owner_Name || "Pika~Kun",
+  ownernumber: process.env.Owner_Number || "916900904828",
   instagramId: process.env.Insta || "8.08_only_mine",
   warns: process.env.Warns_Limits || 3,
-  mongoUrl: process.env.MongoDB || "mongodb+srv://DARKSHAN:1000@cluster0.t1wsjlv.mongodb.net/",
+  mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
   welcome: process.env.Welcome_Msg || '*@$user* joined this group today as $membersth member.\n\n_$prefix welcome off to disable this message._',
   left: process.env.Left_Msg || 'Ex-member *@$user* is no longer available in this group chat.\n\n_$prefix goodbye off to disable this message._',
   promote: process.env.Promote_Msg || '*@$user* has been promoted as an admin in this group.\n\n_$prefix promotem off to disable this message._',
   demote: process.env.Demote_Msg || '*@$user* has been demoted to a member in this group.\n\n_$prefix demotem off to disable this message._',
-  sessionId: process.env.SESSION_ID || "d1UxaUV_Queen-Anya_taEQ=", 
-  auto_read_status : process.env.AUTO_READ_STATUS || "true",
-  alwaysonline: process.env.ALWAYS_ONLINE || "true", 
-  readmessage: process.env.READ_MESSAGE || "true", 
-  auto_status_saver: process.env.AUTO_STATUS_SAVER || "true",
+  sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID", 
   image_1: readFileSync('./lib/Assets/image_1.jpg'), // Thumbnail for allmenu command
   image_2: readFileSync('./lib/Assets/image_2.jpg'), // null image
   image_3: readFileSync("./lib/Assets/image_3.jpg"), // Thumbnail for Dashboard
-  menu_pic: "https://i.ibb.co/PhDcZTM/Thumbnail.png",
   aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
   menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
   badWords: badWords,
@@ -134,3 +101,34 @@ module.exports = {
     banChat: message.banChat
   },
 }
+
+
+
+// Ignore them 👇🏻
+global.botname = process.env.BotName || "Queen Anya" 
+global.author = process.env.Author || "@PikaBotz" 
+global.packname = process.env.PackName || "Queen Anya v2 MD" 
+global.myweb = process.env.Web || "https://github.com/PikaBotz" 
+global.footer = process.env.Footer || "© Queen Anya Bot" 
+global.prefa = process.env.Prefix || ['-'] 
+global.themeemoji = process.env.ThemeEmoji || "🎐" 
+global.ownername = process.env.Owner_Name || "Pika~Kun" 
+global.ownernumber = process.env.Owner_Number || "916900904828" 
+global.adress = process.env.Continent || "Asia, India, Assam" 
+global.timezone = process.env.TimeZone || "Asia/Kolkata" 
+global.instagramId = process.env.Insta || "8.08_only_mine" 
+global.email = process.env.Email_Id || "example@example.com" 
+  
+//--------------- Tip ----------------\\
+global.Tips = [
+`Type *$prefix info* for more information....`,
+`Type *$prefix settings* to commit changes in the bot.`,
+`If you got a bug or error, then please report to developer asap by *$prefix report* command.`
+]
+
+//--------------- Menu images ----------------\\
+global.image_1 = readFileSync('./lib/Assets/image_1.jpg') // Thumbnail for allmenu command
+global.image_2 = readFileSync('./lib/Assets/image_2.jpg') // null image
+global.image_3 = readFileSync("./lib/Assets/image_3.jpg") // Thumbnail for Dashboard
+global.menu_pic = "https://i.ibb.co/PhDcZTM/Thumbnail.png";
+
